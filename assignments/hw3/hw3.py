6934d6bf0476177c0b6f722f18673ed471bc0cad
Carmichaellm <Carmichaellm@g.cofc.edu>
2022-02-04 19:13:40 -0500

    hw3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,8 +1,8 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Liam Carmichael
+hw3.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Develop simple Python programs that do input, produce output and do arithmetic using loops.
 
 Certification of Authenticity:
 <include one of the following>
@@ -12,23 +12,48 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def average():
-    pass
+    grade_entries = eval(input("how many grades will you enter? "))
+    grade_values = 0
+    for i in range(grade_entries):
+        grade_scores = eval(input("Enter grade: "))
+        grade_values = grade_values + grade_scores
+    grade_values = grade_values / grade_entries
+    print("average is",grade_values)
 
 
 def tip_jar():
-    pass
+    tip_total = 0
+    for i in range(5):
+        tip = eval(input("how much would you like to donate? "))
+        tip_total = tip_total + tip
+    print("total tips:", tip_total)
+
 
 
 def newton():
-    pass
+    target = eval(input("What number do you want to square root? "))
+    times =  eval(input("How many times should we improve the approximation? "))
+    approx = target
+    for i in range(times):
+        approx = ((approx + (target / approx)) / 2)
+    print("the square root is approximately", approx)
+    #what the heck is an approx?
+
 
 
 def sequence():
-    pass
+    # terms = eval(input("how many terms would you like? "))
+    # seq = 0
+    # for i in range(terms):
+    #     print(seq,seq,end="\t")
+    #     seq = seq + 2
+    #never seen any math remotely like this, no idea where to even begin
+
 
 
 def pi():
     pass
+#this math is totally alien to me
 
 
 if __name__ == '__main__':
