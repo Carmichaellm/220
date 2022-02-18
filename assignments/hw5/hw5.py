18f80b1cfcfff1a8612cfea6e48a9fa11ab36e09
Carmichaellm <Carmichaellm@g.cofc.edu>
2022-02-18 10:21:18 -0500

    hw5

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -1,42 +1,69 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Liam Carmichael
+hw5.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Index and slice strings and lists in python.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def name_reverse():
-    pass
+    name = input("enter a name (first last): ")
+    name = name.split()
+    print(name[1] + ",", name[0])
 
 
 def company_name():
-    pass
+    domain = input("enter a domain: ")
+    domain = domain.split(".")
+    print(domain[1])
 
 
 def initials():
-    pass
+    students = eval(input("how many students are in the class?"))
+    for i in range(students):
+        print("what is the name of student", i+1, end="")
+        name = input("?")
+        name = name.split()
+        print(name[0][0] + name[1][0])
 
 
 def names():
-    pass
+    names = input("enter a list of names: ")
+    names = names.split(", ")
+    for i in names:
+        i = i.split()
+        print(i[0][0] + i[1][0],end="\t")
 
 
 def thirds():
-    pass
+    sentences = eval(input("enter the number of sentences: "))
+    for i in range(sentences):
+        print("enter sentence", i+1, end="")
+        sent = input(":")
+        print(sent[0:len(sent)+1:3])
+
 
 
 def word_average():
-    pass
+    sentence = input("enter a sentence: ")
+    sentence = sentence.split()
+    average = 0
+    for i in sentence:
+        average = average + len(i)
+    average = average / len(sentence)
+    print(average)
 
 
 def pig_latin():
-    pass
+    sentence = input("enter a sentence to convert to pig latin: ")
+    sentence = sentence.lower()
+    sentence = sentence.split()
+    for i in sentence:
+        i = i[1:len(i)+1] + i[0] + "ay"
+        print(i,end=" ")
 
 
 if __name__ == '__main__':
