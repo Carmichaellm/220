0ff41fab9dd894d8d2e334aa7819d980d890bdda
Carmichaellm <Carmichaellm@g.cofc.edu>
2022-03-04 09:38:13 -0500

    lab6

--- assignments/hw7/hw7.py
+++ assignments/hw7/hw7.py
@@ -10,22 +10,51 @@ I certify that this assignment is entirely my own work.
 
 
 def number_words(in_file_name, out_file_name):
-    order = 0
-    for i in in_file_name:
-        order = order + 1
-        print(order, i)
+    with open(in_file_name) as read:
+        text = read.read()
+    with open(out_file_name, 'w') as write:
+        order = 0
+        sep_wor = text.split()
+        for i in range(len(sep_wor)):
+            order = order + 1
+            out = str(order) + " " + sep_wor
+            write.write(out)
+            write.write('\n')
+
+
+
 
 
 def hourly_wages(in_file_name, out_file_name):
-    pass
+    with open(in_file_name) as read:
+        text = read.readlines()
+    with open(out_file_name, 'w') as write:
+        for i in text:
+            line = i.split()
+            wage = int(line[2]) + 1.65
+            pay = wage * line[3]
+            pay = round(pay, 2)
+            out = line[0] + " " + line[1] + " " + str(pay)
+            write.write(out)
+            write.write('\n')
 
 
 def calc_check_sum(isbn):
-    pass
+    isbn = isbn.replace('-', '')
+    order = 10
+    for i in range(len(isbn)+1):
+        prod = int(isbn[i]) * order
+        sum = sum + prod
+        order = order - 1
+    return sum
+
 
 
 def send_message(file_name, friend_name):
-    pass
+    with open(file_name) as read:
+        text = read.read()
+    with open(friend_name, 'w') as write:
+        write.write(text)
 
 
 def encode():
